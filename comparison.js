d673876e16b6b463206097ea39de3a2e318106e4
mdnazimmia <mdnazimmia2@gmail.com>
2023-04-02 22:30:30 +0200

    comparison.js

---
+++ comparison.js
@@ -0,0 +1,30 @@
+// console.log(5 < 6);
+// console.log(5 > 6);
+// console.log(5 == 6);
+// console.log(6 == 6);
+// console.log(5 != 6);
+// console.log(15 != 15);
+
+// less than or equal
+// console.log(5 <= 6);
+// console.log(6 <= 6);
+// console.log(16 <= 6);
+
+// greather than or equal
+// console.log(5 >= 6);
+// console.log(5 >= 2);
+// console.log(2 >= 2);
+
+var num1 = 36;
+var num2 = 86;
+
+console.log(num1 < num2);
+console.log(num1 > num2);
+console.log(num1 === num2);
+console.log(num1 !== num2);
+console.log(num1 >= num2);
+console.log(num1 <= num2);
+
+// multiple conditions
+// money1 > money2 && result1 > reslut2 && height1 > height2
+// money1 > money2 || reslut1 > reslut2 || height1 > height2
