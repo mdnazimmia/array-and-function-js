7e86fde80e7b1de6caff6678fe0b231e9b58e219
mdnazimmia <mdnazimmia2@gmail.com>
2023-04-02 17:43:11 +0200

    index

---
+++ array.js
@@ -0,0 +1,6 @@
+var friendsAge = [11, 20, 31, 40, 51, 104, 8];
+var picnicFee = [4000, 2000, 5000, 150];
+var names = ['sauda', 'noor', 'nusya', 'nazim'];
+var vowels = ['a', 'e', 'i', 'o', 'u'];
+
+console.log(picnicFee.length);
---
+++ index.js
@@ -0,0 +1,14 @@
+var numbers = [45, 68, 78, 56, 89, 98];
+
+// 1. get element value by index
+var third = numbers[2];
+// console.log(third);
+
+// 2. set element value by index
+numbers[1] = 77;
+numbers[3] = 44;
+// console.log(numbers);
+
+// find index of an element
+var positionIndex = numbers.indexOf(98);
+console.log(positionIndex);
