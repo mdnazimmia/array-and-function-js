e47eb66f3075bbd2b11dd8e544b7f6e9171e5838
mdnazimmia <mdnazimmia2@gmail.com>
2023-04-02 15:10:40 +0200

    first commit

---
+++ variable.js
@@ -0,0 +1,22 @@
+// Numeric or Number type variable
+var glassPrice = 130;
+
+// String type variable
+var specialName = 'Nur Ibnul Arabi';
+
+// boolean type variable
+var isSingle = true;
+
+// tow types of number : Interger: 120;
+// folat, decimal: 20.25;
+
+// Variable naming convention: rules, best practice
+/*
+* 1. single word
+* 2. no gap or space
+* 3. no quote
+* 4. no keyword
+* 5. can not start with a number
+* 6. can not contain  dash
+* 7. should use camelCase   
+*/ 
