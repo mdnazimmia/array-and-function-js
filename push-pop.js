727b8eb4b3e558bf7d702b94fb31b82745595a5f
mdnazimmia <mdnazimmia2@gmail.com>
2023-04-02 18:53:11 +0200

    push and pop

---
+++ push-pop.js
@@ -0,0 +1,24 @@
+var numbers = [78, 45, 98, 69];
+// use push to add element to an array as the last element
+// numbers.push(60);
+// console.log(numbers);
+var friends = ['iqram', 'munna', 'obaidul', 'mostaq'];
+// friends.push('arif');
+console.log(friends);
+
+//  use pop to remove element to an array as the last element
+console.log(numbers);
+numbers.pop();
+var elements = numbers.pop();
+console.log(numbers);
+console.log(elements);
+
+var newone = friends.pop();
+console.log(friends);
+console.log(newone);
+
+numbers.unshift(20);
+console.log(numbers);
+
+friends.unshift('printi');
+console.log(friends);
