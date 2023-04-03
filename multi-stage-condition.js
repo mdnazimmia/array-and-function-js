b938c1699791b0d903a575c02a00305cbe620399
mdnazimmia <mdnazimmia2@gmail.com>
2023-04-03 22:49:40 +0200

    multi-stage-condition

---
+++ multi-stage-condition.js
@@ -0,0 +1,16 @@
+var money = 40;
+var beefPrice = 80;
+var laamPrice = 60;
+var chickenPrice = 30;
+if (beefPrice < money) {
+    console.log('I will order beef');
+}
+else if (laamPrice < money) {
+    console.log('or i will order laam');
+}
+else if (chickenPrice < money) {
+    console.log('or i will order chicken');
+}
+else {
+    console.log('another way i will order smatsch potato and lentinesuppe');
+}
