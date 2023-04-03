b431bf1ca92c269be6bbf0362afdabac50490871
mdnazimmia <mdnazimmia2@gmail.com>
2023-04-03 23:04:57 +0200

    nested-condition

---
+++ nested-conditino.js
@@ -0,0 +1,19 @@
+var math = true;
+var geometry = true;
+var striaghtLine = false;
+if (math == ture) {
+    if (geometry == ture) {
+        if (striaghtLine == ture) {
+
+        }
+        else {
+            console.log('do not go another way')
+        }
+    }
+    else {
+        console.log('pitha gorash hoite parbana');
+    }
+}
+else {
+
+}
