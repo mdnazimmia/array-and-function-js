5b855f2b4a7a7b41d824798aa5dd90c4a1eaf6a2
mdnazimmia <mdnazimmia2@gmail.com>
2023-04-03 16:52:44 +0200

    conditional statement

---
+++ conditional.js
@@ -0,0 +1,33 @@
+var iphonePrice = 79000;
+var myBudget = 95000;
+
+// if iphone price is less then my budget. I will buy the iphone
+/**
+ * if iphone price is less then my budget.
+ *      i will buy the iphone
+ * 
+ * 
+ * if (condition){
+ *      //will execute if the condition is ture
+ * }
+ * */
+
+// if (iphonePrice < myBudget) {
+//     console.log('i will bought the mobile');
+// }
+
+var chickenPrice = 500;
+var myMoney = 50;
+// if (chickenPrice <= myMoney) {
+//     console.log('I will eat chicket');
+// }
+// if (chickenPrice > myMoney) {
+//     console.log('Smash potato and lentils Soup');
+// }
+
+if (chickenPrice < myMoney) {
+    console.log('i will bougth the mobile')
+}
+else {
+    console.log('I will eat smaschpotato');
+}
