9daedd3decfd9c8083698486287b5962a8fd2754
mdnazimmia <mdnazimmia2@gmail.com>
2023-04-03 19:55:31 +0200

    miltiple condition added

--- multiple-condition.js
+++ multiple-condition.js
@@ -1,22 +1,39 @@
-var isGraduated = true;
-var salary = 300000;
-var car = 0;
-// if (isGraduated == true) {
-//     console.log('You can marry me');
+// var isGraduated = true;
+// var salary = 300000;
+// var car = 1;
+// // if (isGraduated == true) {
+// //     console.log('You can marry me');
+// // }
+// // else {
+// //     console.log('Should be wait');
+// // }
+// // if (isGraduated == true && salary > 50000) {
+// //     console.log('You can marry me');
+// // }
+// // else {
+// //     console.log('Should be wait');
+// // }
+
+// if (isGraduated == true && salary > 50000 && car >= 1) {
+//     console.log('You can marry me now');
 // }
 // else {
-//     console.log('Should be wait');
-// }
-// if (isGraduated == true && salary > 50000) {
-//     console.log('You can marry me');
-// }
-// else {
-//     console.log('Should be wait');
+//     console.log('You have to be wait');
 // }
 
-if (isGraduated == ture && salary > 50000 && car > 1) {
-    console.log('You can marry me now');
+var isGraduated = false;
+var salary = 555000;
+car = 1;
+// if (isGraduated == true || salary > 50000) {
+//     console.log('Lets go in a Vecation')
+// }
+// else {
+//     console.log('We would go next year');
+// }
+
+if (isGraduated == true && salary > 500000 || car >= 1) {
+    console.log('I am aggry with your propuse');
 }
 else {
-    console.log('You have to be wait');
+    console.log('I cant aggry with your opinion');
 }
