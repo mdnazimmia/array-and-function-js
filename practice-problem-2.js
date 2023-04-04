42c8962933dacf4dde9680573e814cb91eda932d
mdnazimmia <mdnazimmia2@gmail.com>
2023-04-04 22:27:42 +0200

    second problem solved

---
+++ practice-problem-2.js
@@ -0,0 +1,88 @@
+var myTotalScore = 85;
+var tomsTotalScore = 66;
+var jansTotalScore = 95;
+var peterTotalScore = 56;
+var johnsTotalScore = 40;
+
+if (myTotalScore >= 80) {
+    console.log('A');
+}
+else if (myTotalScore >= 60) {
+    console.log('B');
+}
+else if (myTotalScore >= 50) {
+    console.log('C');
+}
+else if (myTotalScore >= 40) {
+    console.log('D');
+}
+else {
+    console.log('F');
+}
+
+if (tomsTotalScore >= 80) {
+    console.log('A');
+}
+else if (tomsTotalScore >= 60) {
+    console.log('B');
+}
+else if (tomsTotalScore >= 50) {
+    console.log('C');
+}
+else if (tomsTotalScore >= 40) {
+    console.log(D);
+}
+else {
+    console.log(F);
+}
+
+
+if (jansTotalScore >= 80) {
+    console.log('A');
+}
+else if (jansTotalScore >= 60) {
+    console.log('B');
+}
+else if (jansTotalScore >= 50) {
+    console.log('C');
+}
+else if (jansTotalScore >= 40) {
+    console.log('D');
+}
+else {
+    console.log('F');
+}
+
+
+if (peterTotalScore >= 80) {
+    console.log('A');
+}
+else if (peterTotalScore >= 60) {
+    console.log('B');
+}
+else if (peterTotalScore >= 50) {
+    console.log('C');
+}
+else if (peterTotalScore >= 40) {
+    console.log('D');
+}
+else {
+    console.log('F');
+}
+
+
+if (johnsTotalScore >= 80) {
+    console.log('A');
+}
+else if (johnsTotalScore >= 60) {
+    console.log('B');
+}
+else if (johnsTotalScore >= 50) {
+    console.log('C');
+}
+else if (johnsTotalScore >= 40) {
+    console.log('D');
+}
+else {
+    console.log('F');
+}
