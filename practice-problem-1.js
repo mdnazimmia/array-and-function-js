8da6258fe1b8c9edf82579f0bbf18d064521f058
mdnazimmia <mdnazimmia2@gmail.com>
2023-04-04 16:40:42 +0200

    first problem done

---
+++ practice-problem-1.js
@@ -0,0 +1,10 @@
+var fruits = ['Apple', 'Banana', 'Orange'];
+// var second = fruits[1];
+// console.log(second);
+fruits.splice(1, 1, 'Mango');
+console.log(fruits);
+
+fruits.pop('Orange');
+console.log(fruits);
+fruits.push('Watermelon');
+console.log(fruits);
