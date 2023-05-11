6221b0a96e75dca142776ef2f9505022a2d2a22d
mdnazimmia <mdnazimmia2@gmail.com>
2023-05-11 23:53:09 +0200

    multiple-

--- practice.js
+++ practice.js
@@ -133,3 +133,22 @@ if (isGraduated === true || salary > 50000 || car >= 1) {
 else {
     console.log('Please submit your document, we will inform you')
 }
+
+
+
+var money = 40;
+var beefPrice = 80;
+var laamPrice = 60;
+var chickenPrice = 30;
+if (beefPrice < money) {
+    console.log('I will order beef');
+}
+else if (laamPrice < money) {
+    console.log('or i will order laam');
+}
+else if (chickenPrice < money) {
+    console.log('or i will order chicken');
+}
+else {
+    console.log('another way i will order smatsch potato and lentinesuppe');
+}
